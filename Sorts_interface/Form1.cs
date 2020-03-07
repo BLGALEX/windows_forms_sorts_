953dca91aaa0e2631b8122a6a91bc82cb35db2d6
BLGALEX <lepehaleha@bk.ru>
2020-03-07 12:13:43 +1000

    check commit

--- Sorts_interface/Form1.cs
+++ Sorts_interface/Form1.cs
@@ -16,5 +16,10 @@ namespace Sorts_interface
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //
+        }
     }
 }
